b1accb4535df8b2b24c7720d159ae1288b6b12f3
Bondaliname <lina@Lenovo-Thinkpad.>
2025-05-19 14:47:58 +0000

    Solution1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+kubectl create namespace mysql || true
+
+kubectl apply -f st-secret.yml
+
+kubectl apply -f st-configMap.yml
+
+kubectl apply -f st-service.yml
+
+kubectl apply -f statefulSet.yml
+
+kubectl apply -f app-secret.yml
+
+kubectl apply -f deployment.yml
